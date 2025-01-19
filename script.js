62451239ba8fe7ec3116c0049f37ce63a9ea29c4
Cadubandeira <87709064+Cadubandeira@users.noreply.github.com>
2025-01-19 00:28:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -259,6 +259,7 @@ function calculateScores() {
 function showResults() {
     document.getElementById('test-container').classList.add('hidden');
     document.getElementById('results-container').classList.remove('hidden');
+    document.querySelector('#results-container h1').innerText = ` ${userName}, aqui está o resultado do seu teste de personalidade`;
 
     let scores = calculateScores();
 
